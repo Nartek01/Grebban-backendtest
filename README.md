80f5da3d463d781810a3e147b89d746da40b500b
MASTER NARA TEKCHUEN <nartek01@gmail.com>
2023-04-24 10:44:51 +0200

    I give up

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Backend Kodtest för Grebban AB</h1>
+<br>
+<strong>
+NPM version 9.5.1
+Node version 18.16.0
+</strong>
+
+1. Installera npm paket innan med <code>$npm install</code>
+2. <code>$node api.js</code> alternativ <code>node --watch api.js</code>
--- api.js
+++ api.js
@@ -1,146 +1,83 @@
-const express = require('express');
-const api = express();
-const port = 8888;
+/**Importera npm paket att använda package.json använder type:"module" */
+import express from 'express';
+import fetch from 'node-fetch';
 
-const products = [
-    {
-        "id": 6267654,
-        "name": "Auto Omega",
-        "attributes": {
-            "cat": "cat_2",
-            "color": "black,white"
-        }
-    },
-    {
-        "id": 8094994,
-        "name": "Bike Alef",
-        "attributes": {
-            "color": "black,white",
-            "cat": "cat_1"
-        }
-    },
-    {
-        "id": 2846132,
-        "name": "Bike Bet",
-        "attributes": {
-            "color": "blue",
-            "cat": "cat_1_1,cat_1_2"
-        }
-    },
-    {
-        "id": 2169396,
-        "name": "Auto Alpha",
-        "attributes": {
-            "color": "green,blue",
-            "cat": "cat_2_2"
-        }
-    },
-    {
-        "id": 2749899,
-        "name": "Auto Delta",
-        "attributes": {
-            "color": "red",
-            "cat": "cat_2_2"
-        }
-    },
-    {
-        "id": 3311138,
-        "name": "Auto Gamma",
-        "attributes": {
-            "cat": "cat_2_3,cat_2_2",
-            "color": "black,white"
-        }
-    },
-    {
-        "id": 4364807,
-        "name": "Bike Gimel",
-        "attributes": {
-            "color": "red,white,green",
-            "cat": "cat_1_1,cat_1_2"
-        }
-    },
-    {
-        "id": 5385176,
-        "name": "Bike Dalet",
-        "attributes": {
-            "color": "black",
-            "cat": "cat_1_2"
-        }
-    },
-    {
-        "id": 12345,
-        "name": "Random product",
-        "attributes": {}
-    }
-]
+/** Instantiera express klassen och lägger den inuti appi konstant smat sätter port nummer */
+const app = express();
+const port = 3000;
 
-const attribute_meta = [
-    {
-        "name": "Color",
-        "code": "color",
-        "values": [
-            {
-                "name": "Brown",
-                "code": "brown"
-            },
-            {
-                "name": "Black",
-                "code": "black"
-            },
-            {
-                "name": "White",
-                "code": "white"
-            },
-            {
-                "name": "Blue",
-                "code": "blue"
-            },
-            {
-                "name": "Green",
-                "code": "green"
-            },
-            {
-                "name": "Red",
-                "code": "red"
+/** Skapar ett API endpoint med express */
+app.get('/products', (req, res) => {
+
+  fetch('https://draft.grebban.com/backend/products.json') /**Hämtar produkt datan */
+  .then(response => response.json())
+  .then(products => {
+    return fetch('https://draft.grebban.com/backend/attribute_meta.json') /**Hämtar attribut datan samt returnera det så att den är nårbart utanför den lokala scopen */
+      .then(response => response.json())
+      .then(attributes => {
+        /** "Mappa" igenom produkt attributerna */
+        products.forEach(product => {
+          for (let key in product.attributes) {
+            let attributeValue = product.attributes[key];
+            let attribute = attributes.find(attr => attr.code === key);
+            
+            /** Kontrollera att attributerna existera i produkt subet nycklar om det gör det jämför då både nycklarna i båda subset */
+            if (attribute) {
+              let value = attribute.values.find(val => val.code === attributeValue);
+              if (value) { /** Om det nycklarna matchar går ner i dimensioner tills värderna matchar */
+                product.attributes[key] = value.name;
+                if (attribute.code === 'cat') { /**kollar så att värdet börjar med "cat" */
+                  let parentCategory = attributes.find(attr => attr.code === 'cat' && value.code.startsWith(attr.code));
+                  if (parentCategory) { /** Kolla om kategorin har föräldrar */
+                    if (parentCategory.code !== value.code) {
+                      product.attributes[key] = parentCategory.name + '>' + value.name;
+                    } else {
+                      product.attributes[key] = value.name; /** Annars returnera */
+                    }
+                  }
+                }
+              }
             }
-        ]
-    },
-    {
-        "name": "Category",
-        "code": "cat",
-        "values": [
-            {
-                "name": "Electric bikes",
-                "code": "cat_1_1"
-            },
-            {
-                "name": "Bikes",
-                "code": "cat_1"
-            },
-            {
-                "name": "BMX",
-                "code": "cat_1_2"
-            },
-            {
-                "name": "Cars",
-                "code": "cat_2"
-            },
-            {
-                "name": "Electric cars",
-                "code": "cat_2_1"
-            },
-            {
-                "name": "Hybrids",
-                "code": "cat_2_2"
-            },
-            {
-                "name": "Sportscars",
-                "code": "cat_2_3"
+          } /** Kolla om det finns hierarki i category baserad på om det finns komma(",") någonstans i strängen*/
+          if (product.attributes['cat'] && product.attributes['cat'].startsWith('cat_')) {
+            let catValues = product.attributes['cat'].split(',');
+            let newCatValues = [];
+            catValues.forEach(catValue => {
+              let attribute = attributes.find(attr => attr.code === 'cat');
+              let value = attribute.values.find(val => val.code === catValue);
+              if (value) {
+                let parentCategory = attributes.find(attr => attr.code === 'cat' && value.code.startsWith(attr.code));
+                if (parentCategory) {
+                  if (parentCategory.code !== value.code) {
+                    newCatValues.push(parentCategory.name + '>' + value.name);
+                  } else {
+                    newCatValues.push(value.name);
+                  }
+                }
+              }
+            });
+            if (newCatValues.length > 0) {
+              product.attributes['cat'] = newCatValues.join(',');
             }
-        ]
-    }
-]
+          }
+        });
 
-api.listen(port, () => {
-    console.log(`API app listening on port ${port}`);
-})
+        res.send(products);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+      });
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  });
+
+});
+
+
+/** Starta API ändpunkten */
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
